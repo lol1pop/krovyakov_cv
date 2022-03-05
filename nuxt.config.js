d8970de190e8fe78082f800ce21103020bc753b6
Denis Krovyakov <cpl.fassbender@gmail.com>
2022-03-06 04:20:47 +0700

    new build 0.0.5

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/vue-typed', mode: 'client' }
+    '~/plugins/vue-typed'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
