e96bee6b549c845d2541406024a665dce97a1d73
Denis Krovyakov <cpl.fassbender@gmail.com>
2022-03-06 21:25:24 +0700

    new build 0.0.7 add tag <client-only/>

--- docs/_nuxt/static/1646521339/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646521339",layout:"default",error:a,state:{intro:{dataUpdate:new Date(1645635600000)}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2ba4480f"}}}}(null,"\u002F"));
