b1f63d44ce6f11a71ca1e7c42de89e116ffcf893
Denis Krovyakov <cpl.fassbender@gmail.com>
2022-03-06 21:29:55 +0700

    new build 0.0.7

--- docs/_nuxt/static/1646576674/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646576674",layout:"default",error:a,state:{intro:{dataUpdate:new Date(1645635600000)}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"58c294d6"}}}}(null,"\u002F"));
---
+++ docs/_nuxt/static/1646576975/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646576975",layout:"default",error:a,state:{intro:{dataUpdate:new Date(1645635600000)}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0774d70a"}}}}(null,"\u002F"));
